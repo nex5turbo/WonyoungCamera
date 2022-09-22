d08a83f7a87d73d676b8ae415da34c9bb7cd2d37
Wonyoung Jang <nex5turbo@gmail.com>
2022-09-22 20:15:55 +0900

    Initial Commit

---
+++ WonyoungCamera/WonyoungCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonyoungCameraApp.swift
+//  WonyoungCamera
+//
+//  Created by 워뇨옹 on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WonyoungCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
