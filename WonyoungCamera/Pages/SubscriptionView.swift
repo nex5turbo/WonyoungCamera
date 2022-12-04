c4851349a16d1c9a83f3322906724e1fe82c1d4b
Wonyoung Jang <nex5turbo@gmail.com>
2022-12-04 22:50:03 +0900

    구독 구매까지 테스트 완료

--- WonyoungCamera/Pages/SubscriptionView.swift
+++ WonyoungCamera/Pages/SubscriptionView.swift
@@ -6,23 +6,66 @@
 //
 
 import SwiftUI
-
+import SwiftyStoreKit
 struct SubscriptionView: View {
     @Environment(\.dismiss) private var dismiss
+    @ObservedObject var purchaseManager = PurchaseManager.shared
+    @State var isPurchasing = false
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark.circle")
+        ZStack {
+            VStack {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle")
+                    }
                 }
+                .padding()
+                Spacer()
+                Text("Subscription needed")
+                Button {
+                    isPurchasing = true
+                    PurchaseManager.shared.purchaseMonthlyPremium { result in
+                        print(result)
+                        switch result {
+                        case .success:
+                            purchaseManager.setUserPremium(as: true)
+                        case .deferred:
+                            break
+                        case .error:
+                            break
+                        }
+                        isPurchasing = false
+                    }
+                } label: {
+                    Text("Subscribe")
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundColor(.white)
+                        .background(Color.highlightColor)
+                }
+                .padding(.horizontal, 32)
+                .padding(.bottom, 5)
+                Spacer()
+                
             }
-            .padding()
-            Spacer()
-            Text("Subscription needed")
-            Spacer()
-            
+            if isPurchasing {
+                ZStack {
+                    Color.black.opacity(0.4)
+                    VStack {
+                        HStack {
+                            Text("Loading....")
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                        }
+                    }
+                    .padding(20)
+                    .background(.white)
+                    .cornerRadius(10)
+                }
+                .edgesIgnoringSafeArea(.all)
+            }
         }
     }
 }
