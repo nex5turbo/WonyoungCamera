cf18be6d73d0bb98ff9b1c83416648f69170944a
nex5turbo <50979183+nex5turbo@users.noreply.github.com>
2023-01-06 14:03:45 +0900

    Add: `Decoration` model

---
+++ WonyoungCamera/Models/Decoration.swift
@@ -0,0 +1,55 @@
+//
+//  Decoration.swift
+//  WonyoungCamera
+//
+//  Created by Wonyoung Jang on 2023/01/06.
+//
+
+import UIKit
+import Foundation
+
+struct Decoration {
+    var colorFilter: Lut
+    var sticker: String?
+    var background: String?
+    
+    var border: Bool
+    var borderColor: UIColor?
+    
+    var brightness: Float
+    var saturation: Float
+    var contrast: Float
+    
+    init(
+        colorFilter: Lut,
+        sticker: String?,
+        background: String?,
+        border: Bool,
+        borderColor: UIColor?,
+        brightness: Float,
+        saturation: Float,
+        contrast: Float
+    ) {
+        self.colorFilter = colorFilter
+        self.sticker = sticker
+        self.background = background
+        self.border = border
+        self.borderColor = borderColor
+        self.brightness = brightness
+        self.saturation = saturation
+        self.contrast = contrast
+    }
+
+    static func empty() -> Self {
+        return Decoration(
+            colorFilter: .Natural,
+            sticker: nil,
+            background: nil,
+            border: false,
+            borderColor: nil,
+            brightness: 0.5,
+            saturation: 0.5,
+            contrast: 0.5
+        )
+    }
+}
