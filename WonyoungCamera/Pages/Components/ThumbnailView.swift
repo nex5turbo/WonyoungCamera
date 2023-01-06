8b383eb73c06ca1f9f0879292f4a9e14b170403a
nex5turbo <50979183+nex5turbo@users.noreply.github.com>
2023-01-06 15:06:37 +0900

    Add: ThumbnailView

---
+++ WonyoungCamera/Pages/Components/ThumbnailView.swift
@@ -0,0 +1,21 @@
+//
+//  ThumbnailView.swift
+//  WonyoungCamera
+//
+//  Created by Wonyoung Jang on 2023/01/06.
+//
+
+import SwiftUI
+
+struct ThumbnailView: View {
+    var path: String
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThumbnailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThumbnailView(path: "")
+    }
+}
