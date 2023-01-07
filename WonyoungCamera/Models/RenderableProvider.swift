a8cc2921b7fb9195d66483bdb65582b27841ea6a
Wonyoung Jang <nex5turbo@gmail.com>
2023-01-08 00:06:52 +0900

    concurrent dictionary

--- WonyoungCamera/Models/RenderableProvider.swift
+++ WonyoungCamera/Models/RenderableProvider.swift
@@ -9,23 +9,39 @@ import Foundation
 import Metal
 
 class RenderableProvider {
-    private var cacheMap: [String: Renderable] = [:]
+    private var cacheMap = ConcurrentDictionary<String, Renderable>()
     private var operationQueue: OperationQueue = {
         let operationQueue = OperationQueue()
         operationQueue.maxConcurrentOperationCount = 8
         return operationQueue
     }()
     
+    deinit {
+        stop()
+    }
+
+    public func stop() {
+        operationQueue.cancelAllOperations()
+        /*
+         operationQueue.maxConcurrentOperationCount = 0 을 없애면
+         stop 이후에 enqueue가 호출되고 gif, video 같은 renderable이 살아 있을 수 있는 여지가 있습니다.
+         operationQueue.maxConcurrentOperationCount = 0 을 사용한다면
+         TODO: cancel 요청된 이후 마무리 작업을 하지 못해 operation이 쌓이게 되어 memory leak이 발생하여 해결해야 합니다.
+         
+         현재는 renderable이 계속 도는 것보다 operation leak이 낫다고 판단됩니다.
+         */
+        operationQueue.maxConcurrentOperationCount = 0
+        cacheMap.removeAll()
+    }
+    
     func getRenderableOrFetch(_ decorator: Decorator) -> Renderable? {
         guard let renderable = cacheMap[decorator.id] else {
+            enqueue(decorator)
             return nil
         }
         return renderable
     }
-    func setRenderable() {
-        
-    }
-    
+
     private func isInQueue(_ decorator: Decorator) -> Bool {
         for operation in operationQueue.operations {
             guard let operation = operation as? ProvidingOperation else { continue }
@@ -34,12 +50,10 @@ class RenderableProvider {
         return false
     }
 
-    /// 같은 operation이 있다면 enqueue하지 않는다.
     private func enqueue(_ decorator: Decorator) {
         guard isInQueue(decorator) == false else { return }
         let operation = ProvidingOperation(decorator) { [weak self] (renderable) in
             guard let self = self else { return }
-            // 현재는 renderable이 nil인 경우에도 set 해주고 있습니다. 이것이 추후 문제가 된다면 수정되어야 할 수도 있습니다.
             self.set(renderable, key: decorator.id)
         }
         operationQueue.addOperation(operation)
