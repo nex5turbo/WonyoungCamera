eb15858d08eb74f6fe3b427281037199da1282f4
Wonyoung Jang <nex5turbo@gmail.com>
2023-01-07 21:02:17 +0900

    ThumbnailView

--- WonyoungCamera/Pages/Components/ThumbnailView.swift
+++ WonyoungCamera/Pages/Components/ThumbnailView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ThumbnailView: View {
     var path: String
+    @State private var thumbnail: UIImage?
+    var preferredThumbnailSize: CGFloat {
+        return 480
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if let thumbnail {
+                Image(uiImage: thumbnail)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .task {
+            guard let url = URL(string: path) else {
+                return
+            }
+            guard let data = try? Data(contentsOf: url) else {
+                return
+            }
+            guard let image = UIImage(data: data) else {
+                return
+            }
+            let scaleRatio = preferredThumbnailSize / image.size.width
+            if let scaledImage = image.preparingThumbnail(of: image.size *= scaleRatio) {
+                thumbnail = scaledImage
+            } else {
+                thumbnail = image
+            }
+        }
     }
 }
 
