33cdb2fb6a922516bc30e8f70ebb43a9ac67ec71
Wonyoung Jang <nex5turbo@gmail.com>
2023-01-17 22:26:14 +0900

    remove adjustment, decoration variable fixed

--- WonyoungCamera/Models/Decoration.swift
+++ WonyoungCamera/Models/Decoration.swift
@@ -19,6 +19,16 @@ struct Decoration {
     var brightness: Float
     var saturation: Float
     var contrast: Float
+/**
+ var sharpness: Float
+ var highlights: Float
+ var shadows: Float
+ var exposure: Float
+  whiteBalance
+ var vibrance: Float
+  vignette
+ var grain: Float
+*/
     var scale: Float
     
     init(
@@ -79,3 +89,34 @@ struct Decoration {
         }
     }
 }
+/**
+    Contrast -> Float
+    Sharpness -> Float
+    Highlights, shadows -> Float, Float
+    Exposure -> Float
+    White Balance -> WhiteBalanceProperties(Float, Float)
+    Vibrance -> Float
+    Vignette -> struct VignetteUniform {
+            let vignetteCenter: simd_float2
+            let vignetteColor: simd_float3
+            let vignetteStart: Float
+            let vignetteEnd: Float
+            let vignettePercent: Float
+            init(vignettePercent: Float,
+                 vignetteCenter: simd_float2 = simd_float2(x: 0.5, y: 0.5),
+                 vignetteColor: simd_float3 = simd_float3(repeating: 0),
+                 vignetteStart: Float = 0.3,
+                 vignetteEnd: Float = 0.75) {
+                self.vignettePercent = vignettePercent
+                self.vignetteCenter = vignetteCenter
+                self.vignetteColor = vignetteColor
+                self.vignetteStart = vignetteStart
+                self.vignetteEnd = vignetteEnd
+            }
+        }
+
+    Grain -> Float
+    Gray scale -> Not
+    Saturation -> Float
+    Brightness -> Float
+*/
