4ccf7a20822d73c6d3dcb6c891a55109f81b46e0
Wonyoung Jang <nex5turbo@gmail.com>
2024-03-12 23:49:10 +0900

    background image 추가

--- WonyoungCamera/Models/Backgrounds.swift
+++ WonyoungCamera/Models/Backgrounds.swift
@@ -9,6 +9,7 @@ import Foundation
 import MetalKit
 
 enum Backgrounds: String, CaseIterable {
+    case crb1, crb2, crb3, crb4, crb5, crb6
     case a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11
     case a12, a13, a14, a15, a16, a17, a18, a19, a20
     case a21, a22, a23, a24, a25, a26, a27, a28, a29, a30
